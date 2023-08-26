77f3609ae2e89b31ce9ef8b8d4fe5503c3551349
nhh1500 <nhh1500@gmail.com>
2023-08-26 23:09:18 +0800

    Update multiple_snapHeights_modalbottomsheet.dart

--- lib/multiple_snapHeights_modalbottomsheet.dart
+++ lib/multiple_snapHeights_modalbottomsheet.dart
@@ -5,4 +5,6 @@ library multiple_stoppoints_modalbottomsheet;
 
 export 'src/multiple_snapHeights_modalbottomsheet.dart';
 export 'src/multiple_snapHeights_modalbottomsheet_widget.dart';
+export 'src/model/snapHeight.dart';
+export 'src/model/snapStatus.dart';
 // TODO: Export any libraries intended for clients of this package.
